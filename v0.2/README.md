76ec6effc7b497ffcb0c513c4a9b8302938e5278
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-10-27 18:46:40 +0200

    fixed certificate, and peerjs

--- v0.2/README.md
+++ v0.2/README.md
@@ -4,3 +4,11 @@
 - npm i 
 - npm i -g nodemon
 - npm i -g peerjs
+
+## START PEER SERVER
+peerjs --port 9000
+
+TODO:
+- static ip aan server geven
+- certificate geven aan dit ip adress
+- testen op andere apparaten
--- v0.2/server.js
+++ v0.2/server.js
@@ -2,49 +2,53 @@ import express from "express";
 const app = express();
 import https from "https";
 import * as fs from "fs";
-import { v4 as uuidV4 } from 'uuid';
+import {
+	v4 as uuidV4
+} from 'uuid';
 import * as ioTool from "socket.io";
 
 const port = 3000;
 const httpsOptions = {
-  key: fs.readFileSync('./security/cert.key'),
-  cert: fs.readFileSync('./security/cert.pem')
+	key: fs.readFileSync('./security/cert.key'),
+	cert: fs.readFileSync('./security/cert.pem')
 }
 const server = https.createServer(httpsOptions, app)
 const io = new ioTool.Server(server);
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+	res.redirect(`/${uuidV4()}`)
 })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+	res.redirect(`/${uuidV4()}`)
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
+	res.render('room', {
+		roomId: req.params.room
+	})
 })
 
 server.listen(port, () => {
-  console.log('server running at ' + `https://localhost:${port}`);
+	console.log('server running at ' + `https://localhost:${port}`);
+})
+
+io.on('connection', socket => {
+	socket.on('join-room', (roomId, userId) => {
+		socket.join(roomId);
+		socket.to(roomId).emit('user-connected', userId)
+
+		socket.on('disconnect', () => {
+			socket.to(roomId).emit('user-disconnected', userId)
+		})
+	})
 })
